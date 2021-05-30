acc65b2d21e161eb3ab40e8bb46f7b24a06bcd20
barbarabodinier <b.bodinier@imperial.ac.uk>
2021-05-30 12:49:20 +0200

    update package version

--- 1-illustrations/1-regression.R
+++ 1-illustrations/1-regression.R
@@ -1,138 +1,204 @@
-rm(list=ls())
+rm(list = ls())
 setwd("~/Dropbox/Stability_selection/")
 
 library(focus)
 
 # Parameters
-n=100
-pk=50
-nu_pred=0.2 # density of predictors among variables
-sd_pred_error=2
-seed=1
-PFER_thr=10
+n <- 100
+pk <- 50
+seed <- 1
+PFER_thr <- 10
 
 # Simulation
 set.seed(seed)
-simul=SimulateXY(n=n, pk=pk, nu_pred=nu_pred, family="gaussian", 
-                 sd_pred_error=sd_pred_error, continuous=FALSE)
-table(simul$theta_pred)
+simul <- SimulateRegression(
+  n = n, pk = pk, theta = c(rep(1, 10), rep(0, 40)),
+  family = "gaussian", continuous = FALSE, prop_ev = 0.6
+)
+table(simul$theta)
 
-# Visualisation of the simulated error 
-par(mar=c(5,5,1,1))
-plot(simul$Y_pred, simul$Y, pch=19, col="navy", 
-     xlab=expression(X), ylab=expression(Y), cex.lab=1.5)
+# Visualisation of the simulated error
+par(mar = c(5, 5, 1, 1))
+plot(simul$Y_pred, simul$Y,
+  pch = 19, col = "navy",
+  xlab = expression(X), ylab = expression(Y), cex.lab = 1.5
+)
 
 # Stability selection for variable selection
-system.time({out=VariableSelection(xdata=simul$X, ydata=simul$Y, family="gaussian")})
+system.time({
+  out <- VariableSelection(xdata = simul$X, ydata = simul$Y, family = "gaussian", lambda.min.ratio = 1e-2)
+})
 
-A=SelectedVariables(out)
-SelectionPerformance(A,simul$theta_pred)
-selprop=SelectionProportions(out)
+# Extracting selection proportions
+A <- SelectedVariables(out)
+SelectionPerformance(A, simul$theta)
+selprop <- SelectionProportions(out)
 
 # Re-ordering the selection proportions
-ids=sort.list(selprop, decreasing=TRUE)
-Asum=A+2*simul$theta_pred
-selprop=selprop[ids]
-Asum=Asum[ids]
+ids <- sort.list(selprop, decreasing = TRUE)
+Asum <- A + 2 * simul$theta
+selprop <- selprop[ids]
+Asum <- Asum[ids]
 
 # Figure parameters
-mylwd=3
-mycolours=c("grey", "tomato", "forestgreen", "navy") # FP: red, TP: navy, FN: forestgreen
-names(mycolours)=c("TN", "FP", "FN", "TP")
-mar=c(5,5,5,7)
+mylwd <- 3
+mycolours <- c("grey", "tomato", "forestgreen", "navy") # FP: red, TP: navy, FN: forestgreen
+names(mycolours) <- c("TN", "FP", "FN", "TP")
+mar <- c(5, 5, 5, 7)
 
 # Saving figure
-{pdf("Figures/1-illustrations/Figure1_variable_selection.pdf", width=15, height=12)
+{
+  pdf("Figures/1-illustrations/Figure1_variable_selection.pdf", width = 15, height = 12)
   # Calibration plot
-  CalibrationPlot(out, mar=mar, mfrow=c(2,1))
-  mtext(text="A", cex=4, side=3, at=-10, line=0)
-  
+  par(mfrow = c(2, 1), mar = mar)
+  CalibrationPlot(out)
+  mtext(text = "A", cex = 4, side = 3, at = -9, line = 0)
+
   # Selection proportions
-  par(mar=mar)
-  plot(selprop, type="h", col=mycolours[Asum+1], las=1, xaxt="n", ylim=c(0,1),
-       xlab="", ylab="Selection Proportion", cex.lab=1.5, lwd=mylwd, bty="n")
-  abline(h=GetArgmax(out)[2], lty=2, col="darkred")
-  abline(h=1-GetArgmax(out)[2], lty=2, col="orange")
-  axis(side=1, at=1:length(selprop), labels=NA)
-  for (k in 1:length(selprop)){
-    axis(side=1, at=k, labels=names(selprop)[k], las=2, cex.axis=0.8, tick=FALSE)
+  plot(selprop,
+    type = "h", col = mycolours[Asum + 1], las = 1, xaxt = "n", ylim = c(0, 1),
+    xlab = "", ylab = "Selection Proportion", cex.lab = 1.5, lwd = mylwd, bty = "n"
+  )
+  abline(h = Argmax(out)[2], lty = 2, col = "darkred")
+  abline(h = 1 - Argmax(out)[2], lty = 2, col = "orange")
+  axis(side = 1, at = 1:length(selprop), labels = NA)
+  for (k in 1:length(selprop)) {
+    axis(side = 1, at = k, labels = names(selprop)[k], las = 2, cex.axis = 0.8, tick = FALSE)
   }
-  legend("right", lty=c(rep(1,4),2,2), lwd=mylwd, bg="white", bty="n",
-         col=c(mycolours[c(1,4,2,3)], "darkred", "orange"), cex=1.2,
-         legend=c(paste0("True Negatives (N=", sum(Asum==0), ")"),
-                  paste0("True Positives (N=", sum(Asum==3), ")"),
-                  paste0("False Positives (N=", sum(Asum==1), ")"),
-                  paste0("False Negatives (N=", sum(Asum==2), ")"),
-                  eval(parse(text="expression(hat(pi))")), 
-                  eval(parse(text="expression(1-hat(pi))"))))
-  mtext(text="B", cex=4, side=3, at=-4, line=0)
-  dev.off()}
+  legend("right",
+    lty = c(rep(1, 4), 2, 2), lwd = mylwd, bg = "white", bty = "n",
+    col = c(mycolours[c(1, 4, 2, 3)], "darkred", "orange"), cex = 1.2,
+    legend = c(
+      paste0("True Negatives (N=", sum(Asum == 0), ")"),
+      paste0("True Positives (N=", sum(Asum == 3), ")"),
+      paste0("False Positives (N=", sum(Asum == 1), ")"),
+      paste0("False Negatives (N=", sum(Asum == 2), ")"),
+      eval(parse(text = "expression(hat(pi))")),
+      eval(parse(text = "expression(1-hat(pi))"))
+    )
+  )
+  mtext(text = "B", cex = 4, side = 3, at = -4, line = 0)
+  dev.off()
+}
 
 
 # Constrained calibration
-system.time({out_constr=VariableSelection(xdata=simul$X, ydata=simul$Y, family="gaussian", PFER_thr=PFER_thr)})
+system.time({
+  out_constr <- VariableSelection(xdata = simul$X, ydata = simul$Y, family = "gaussian", PFER_thr = PFER_thr, lambda.min.ratio = 1e-2)
+})
 
 # Saving figure
-{pdf("Figures/1-illustrations/Constrained_calibration.pdf", width=15, height=12)
-  par(mfrow=c(2,1), mar=c(1,5,4.2,7))
-  b=1
-  stability=out_constr
-  mat=stability$S_2d
+{
+  pdf("Figures/1-illustrations/Constrained_calibration.pdf", width = 10, height = 11)
+  par(mfrow = c(3, 1), mar = c(1, 6, 4.2, 7))
+  b <- 1
+  stability <- out_constr
+  mat <- stability$S_2d
   # ids=which(apply(mat,1,FUN=function(x){any(!is.na(x))}))
-  ids=1:nrow(mat)
-  mat=mat[ids,,drop=FALSE]
-  mat=mat[,,drop=FALSE]
-  colnames(mat)=stability$params$pi_list
-  rownames(mat)=formatC(stability$Lambda[,b], format="e", digits=2)[ids]
-  
-  # Extracting corresponding numbers of selected variables (q)
-  Q=stability$Q[,b]
-  Q=Q[ids]
-  
-  # Heatmap representation
-  Heatmap(mat[nrow(mat):1,ncol(mat):1])
-  
-  # Identifying best pair of parameters
-  graphics::abline(h=which.min(abs(as.numeric(colnames(mat))-GetArgmax(stability)[b,2]))-0.5, 
-                   lty=3, col="red")
-  graphics::abline(v=nrow(mat)-which(stability$Lambda[ids,b]==GetArgmax(stability)[b,1])+0.5, 
-                   lty=3, col="red")
-  
-  # Including axes
-  graphics::axis(side=1, at=(1:nrow(mat))-0.5, las=2, labels=rev(rownames(mat)))
-  graphics::axis(side=2, at=(1:ncol(mat))-0.5, las=2,
-                 labels=formatC(as.numeric(colnames(mat)), format="f", digits=2))
-  graphics::axis(side=3, at=(1:nrow(mat))-0.5, las=2,
-                 labels=rev(formatC(Q, format="f", big.mark=",", digits=0)))
-  
-  # Including axis labels
-  graphics::mtext(text=expression(pi), side=2, line=3.5, cex=1.5)
-  graphics::mtext(text=expression(italic(q)), side=3, line=2.5, cex=1.5)
-  mtext(text=expression(lambda), cex=1.5, side=1, at=-3, line=2.3)
-  mtext(text="A", cex=4, side=3, at=-10, line=0)
-  
-  # Uncontrained curve
-  mat=out$S_2d
-  vect=rev(apply(mat,1,max,na.rm=TRUE))
-  vect[is.infinite(vect)]=NA
-  plot(seq(1,length(vect))-0.5, vect, cex.lab=1.5, pch=19, cex=0.25, col="navy",
-       xaxt="n", xlab="", ylab="Stability Score", xlim=c(0,length(vect)), bty="n")
-  axis(side=3, at=seq(1,length(vect))-0.5, labels=NA)
-  axis(side=1, at=seq(1,length(vect))-0.5, labels=NA)
-  lines(seq(1,length(vect))-0.5, vect, col="navy")
-  abline(v=length(vect)-GetArgmaxId(out)[1]+0.5, col="navy", lty=3)
-  mtext(text="B", cex=4, side=3, at=-10, line=0)
-  
-  # Constrained curve
-  mat=out_constr$S_2d
-  ids=which(apply(mat,1,FUN=function(x){any(!is.na(x))}))
-  vect=apply(mat[ids,],1,max,na.rm=TRUE)
-  points(nrow(mat)-ids+0.5, vect, col="red", pch=19, cex=0.25)
-  lines(nrow(mat)-ids+0.5, vect, col="red", lty=2)
-  abline(v=nrow(mat)-GetArgmaxId(out_constr)[1]+0.5, col="red", lty=3)
-  legend("topleft", lty=c(1,2), col=c("navy", "red"), pch=19, pt.cex=0.25,
-         bty="n", cex=1.2, 
-         legend=c("Unconstrained calibration", 
-                  paste0("Constrained calibration PFER<",PFER_thr)))
-  dev.off()}
+  ids <- 1:nrow(mat)
+  mat <- mat[ids, , drop = FALSE]
+  mat <- mat[, , drop = FALSE]
+  colnames(mat) <- stability$params$pi_list
+  rownames(mat) <- formatC(stability$Lambda[, b], format = "e", digits = 2)[ids]
 
+  # Extracting corresponding numbers of selected variables (q)
+  Q <- stability$Q[, b]
+  Q <- Q[ids]
+
+  # Heatmap representation
+  Heatmap(mat[nrow(mat):1, ncol(mat):1], axes = FALSE)
+
+  # Identifying best pair of parameters
+  graphics::abline(
+    h = which.min(abs(as.numeric(colnames(mat)) - Argmax(stability)[b, 2])) - 0.5,
+    lty = 3, col = "red"
+  )
+  graphics::abline(
+    v = nrow(mat) - which(stability$Lambda[ids, b] == Argmax(stability)[b, 1]) + 0.5,
+    lty = 3, col = "red"
+  )
+
+  # Including axes
+  graphics::axis(side = 1, at = (1:nrow(mat)) - 0.5, las = 2, labels = rev(rownames(mat)))
+  graphics::axis(
+    side = 2, at = (1:ncol(mat)) - 0.5, las = 2,
+    labels = formatC(as.numeric(colnames(mat)), format = "f", digits = 2)
+  )
+  graphics::axis(
+    side = 3, at = (1:nrow(mat)) - 0.5, las = 2,
+    labels = rev(formatC(Q, format = "f", big.mark = ",", digits = 0))
+  )
+
+  # Including axis labels
+  graphics::mtext(text = expression(pi), side = 2, line = 3.5, cex = 1.5)
+  graphics::mtext(text = expression(italic(q)), side = 3, line = 2.5, cex = 1.5)
+  mtext(text = expression(lambda), cex = 1.5, side = 1, at = -3, line = 2.3)
+  mtext(text = "A", cex = 3.5, side = 3, at = -10, line = 0)
+
+  # Uncontrained curve
+  mat <- out$S_2d
+  vect <- rev(apply(mat, 1, max, na.rm = TRUE))
+  vect[is.infinite(vect)] <- NA
+  plot(seq(1, length(vect)) - 0.5, vect,
+    cex.lab = 1.5, pch = 19, cex = 0.25, col = "navy",
+    xaxt = "n", xlab = "", ylab = "Stability Score", xlim = c(0, length(vect)), bty = "n"
+  )
+  axis(side = 3, at = seq(1, length(vect)) - 0.5, labels = NA)
+  axis(side = 1, at = c(1, length(vect)) - 0.5, labels = NA)
+  lines(seq(1, length(vect)) - 0.5, vect, col = "navy")
+  abline(v = length(vect) - ArgmaxId(out)[1] + 0.5, col = "navy", lty = 3)
+  mtext(text = "B", cex = 3.5, side = 3, at = -10, line = 0.5)
+
+  # Constrained curve
+  mat <- out_constr$S_2d
+  ids <- which(apply(mat, 1, FUN = function(x) {
+    any(!is.na(x))
+  }))
+  vect <- apply(mat[ids, ], 1, max, na.rm = TRUE)
+  points(nrow(mat) - ids + 0.5, vect, col = "red", pch = 19, cex = 0.25)
+  lines(nrow(mat) - ids + 0.5, vect, col = "red", lty = 2)
+  abline(v = nrow(mat) - ArgmaxId(out_constr)[1] + 0.5, col = "red", lty = 3)
+  legend("topleft",
+    lty = c(1, 2), col = c("navy", "red"), pch = 19, pt.cex = 0.25,
+    bty = "n", cex = 1.2,
+    legend = c(
+      "Unconstrained calibration",
+      paste0("Constrained calibration PFER<", PFER_thr)
+    )
+  )
+
+  # Extracting and re-ordering the selection proportions
+  A <- SelectedVariables(out_constr)
+  selprop <- SelectionProportions(out_constr)
+  ids <- sort.list(selprop, decreasing = TRUE)
+  Asum <- A + 2 * simul$theta
+  selprop <- selprop[ids]
+  Asum <- Asum[ids]
+
+  # Selection proportions
+  par(mar = c(4.2, 6, 1, 7))
+  plot(selprop,
+    type = "h", col = mycolours[Asum + 1], las = 1, xaxt = "n", ylim = c(0, 1),
+    xlab = "", ylab = "Selection Proportion", cex.lab = 1.5, lwd = mylwd, bty = "n"
+  )
+  abline(h = Argmax(out_constr)[2], lty = 2, col = "darkred")
+  abline(h = 1 - Argmax(out_constr)[2], lty = 2, col = "orange")
+  axis(side = 1, at = 1:length(selprop), labels = NA)
+  for (k in 1:length(selprop)) {
+    axis(side = 1, at = k, labels = names(selprop)[k], las = 2, cex.axis = 1, tick = FALSE)
+  }
+  legend("right",
+    lty = c(rep(1, 4), 2, 2), lwd = mylwd, bg = "white", bty = "n",
+    col = c(mycolours[c(1, 4, 2, 3)], "darkred", "orange"), cex = 1.2,
+    legend = c(
+      paste0("True Negatives (N=", sum(Asum == 0), ")"),
+      paste0("True Positives (N=", sum(Asum == 3), ")"),
+      paste0("False Positives (N=", sum(Asum == 1), ")"),
+      paste0("False Negatives (N=", sum(Asum == 2), ")"),
+      eval(parse(text = "expression(hat(pi))")),
+      eval(parse(text = "expression(1-hat(pi))"))
+    )
+  )
+  mtext(text = "C", cex = 3.5, side = 3, at = -4, line = -2.7)
+  dev.off()
+}
--- 1-illustrations/2-graphical_lasso.R
+++ 1-illustrations/2-graphical_lasso.R
@@ -1,50 +1,56 @@
-rm(list=ls())
+rm(list = ls())
 setwd("~/Dropbox/Stability_selection/")
 
 library(focus)
 library(igraph)
 
-
 # Simulation parameters
-n=100
-pk=50
-topology="scale-free"
-if (topology!="scale-free"){
-  N=(sum(pk)*(sum(pk)-1))/2
-  print(paste("Number of edges:", nu*N))
+n <- 100
+pk <- 50
+topology <- "scale-free"
+if (topology != "scale-free") {
+  N <- (sum(pk) * (sum(pk) - 1)) / 2
+  print(paste("Number of edges:", nu * N))
 } else {
-  print(paste("Number of edges:", sum(pk)-1))
+  print(paste("Number of edges:", sum(pk) - 1))
 }
-seed=0
+seed <- 2
 
 # Simulation of data with underlying graph structure
 set.seed(seed)
-simul=SimulateGraph(n=n, pk=pk, topology=topology, nu=nu)
+simul <- SimulateGraphical(n = n, pk = pk, topology = topology, output_matrices = TRUE)
 
 # Stability selection graphical LASSO
-out=GraphicalModel(data=simul$data, PFER_thr=10, PFER_method="MB")
-A=Adjacency(out)
+out <- GraphicalModel(xdata = simul$data, PFER_thr = 20, PFER_method = "MB")
+A <- Adjacency(out)
 
 # Evaluating the selection performances
-perf=SelectionPerformance(A, simul$theta)
-mygraph=SelectionPerformanceGraph(A, simul$theta)
+perf <- SelectionPerformance(A, simul$theta)
+perf
+mygraph <- SelectionPerformanceGraph(A, simul$theta)
 
 # Saving figure
-{pdf("Figures/1-illustrations/Figure1_graphical_model.pdf", width=15, height=7)
+{
+  pdf("Figures/1-illustrations/Figure1_graphical_model.pdf", width = 15, height = 7)
+  par(mfrow = c(1, 2), mar = c(7, 5, 5, 5))
+
   # Calibration plot
-  CalibrationPlot(out, mfrow=c(1,2), mar=c(7,5,5,5))
-  mtext(text="C", cex=4, side=3, at=-9, line=0)
-  
+  CalibrationPlot(out)
+  mtext(text = "C", cex = 4, side = 3, at = -9, line = 0)
+
   # Graph representation of the detected and missed edges
-  set.seed(1)
-  par(mar=c(2,2,2,0))
-  plot(mygraph, layout=layout_with_fr(mygraph))
-  mtext(text="D", cex=4, side=3, at=-1, line=-3)
-  legend(x=-1.2,y=-0.2, col=c("navy", "tomato", "forestgreen"), 
-         lty=c(1,2,3), bty="n", cex=1.2,
-         legend=c(paste0("True Positives (N=",perf$TP,")"),
-                  paste0("False Positives (N=",perf$FP,")"),
-                  paste0("False Negatives (N=",perf$FN,")")))
-  dev.off()}
-
-
+  par(mar = c(2, 2, 2, 0))
+  set.seed(2)
+  plot(mygraph, layout = layout_with_fr(mygraph))
+  mtext(text = "D", cex = 4, side = 3, at = -1, line = -3)
+  legend(
+    x = -1.2, y = -0.5, col = c("navy", "tomato", "forestgreen"),
+    lty = c(1, 2, 3), bty = "n", cex = 1.2,
+    legend = c(
+      paste0("True Positives (N=", perf$TP, ")"),
+      paste0("False Positives (N=", perf$FP, ")"),
+      paste0("False Negatives (N=", perf$FN, ")")
+    )
+  )
+  dev.off()
+}
